015b30f5b9a5c37db261b52de8da5ed41fe6fb0a
AlejanPelaez9004 <alejanpelaez@knights.ucf.edu>
2021-08-31 14:25:15 -0700

    Completed!

--- src/main/java/Pelaez/Alejandro/App.java
+++ src/main/java/Pelaez/Alejandro/App.java
@@ -3,15 +3,38 @@
  *  Copyright 2021 Alejandro Pelaez
  */
 package Pelaez.Alejandro;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args)
     {
-        System.out.print("What is your name?");
+        final float taxRate = 0.055f;
+
         Scanner input = new Scanner(System.in);
-        String name = input.next();
-        System.out.print("Hello " + name + ", nice to meet you!");
-        System.out.print("\n");
+        float total;
+        float addTax;
+        float prices = 0.0f;
+        int loopCount = 0;
+
+        String cont = "";
+        while (!"n".equals(cont))
+        {
+            loopCount++;
+            System.out.printf("Enter the price of item %d: ", loopCount);
+            float p = input.nextFloat();
+            System.out.printf("Enter the quantity of item %d: ", loopCount);
+            float q = input.nextFloat();
+            System.out.print("Would you like to add another item? (y/n) ");
+
+            prices += p * q;
+            cont = input.next();
+        }
+        addTax = prices * taxRate;
+        total = addTax + prices;
+
+        System.out.printf("Subtotal: $%.2f", prices);
+        System.out.printf("\nTax: $%.2f", addTax);
+        System.out.printf("\nTotal: $%.2f\n", total);
     }
 }
